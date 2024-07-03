3dbd6fbd08f5d95b3cdac5225aa252803912cb32
Aatmun Baxi <aatmun@pop-os.localdomain>
2024-07-02 21:38:44 -0500

    Add a bunch of color manipulations

--- technicolor.el
+++ technicolor.el
@@ -29,7 +29,12 @@
 ;; palette, which would then access the correct theme-dependent color
 ;; that the user specifies.
 ;;
+;; In addition, it provides an abstraction above the `color' library
+;; for basic color manipulation, allowing for more ergonomic retrieval
+;; and usage of theme-specific colors.
+;;
 ;;; Code:
+
 (defgroup technicolor nil
   "Almost univeral color palette access."
   :group 'technicolor)
@@ -78,8 +83,8 @@ you want the color `red' in `technicolor-colors' to map to
 
 ;;;###autoload
 (defvar technicolor-standard-themes-data
-  '("^ef-.*" standard-themes-get-color-value ((foreground . fg-main)
-                                              (background . bg-main)))
+  '("^standard-.*" standard-themes-get-color-value ((foreground . fg-main)
+                                                    (background . bg-main)))
   "Default configuration for `standard-themes'.")
 
 ;;;###autoload
@@ -96,11 +101,14 @@ in all themes matched in `technicolor-themes-alist'."
   :type '(list symbol)
   :group 'technicolor)
 
+
+(require 'color)
+
 (defun technicolor--get-catppuccin-color (color)
   (let ((ctp-theme-colors (intern
                            (concat "catppuccin-" (symbol-name catppuccin-flavor) "-colors"))))
-    (alist-get color (eval ctp-theme-colors)))
-  )
+    (alist-get color (eval ctp-theme-colors))))
+
 
 (defun technicolor--get-theme-data (theme)
   (let ((theme-name (symbol-name theme))
@@ -118,11 +126,91 @@ in all themes matched in `technicolor-themes-alist'."
 
   COLOR should appear in `technicolor-colors' or be universally
 available in all themes known to technicolor."
-  (pcase-let* ((`(_  ,accessor ,color-mapping) (technicolor--get-theme-data (car custom-enabled-themes)))
-               (theme-color (if (alist-get color color-mapping)
-                                (alist-get color color-mapping)
-                              color)))
-    (funcall accessor theme-color)))
+  (cond ((and (not (null color)) (symbolp color))
+         (pcase-let* ((`(_  ,accessor ,color-mapping) (technicolor--get-theme-data (car custom-enabled-themes)))
+                      (theme-color (if (assoc color color-mapping)
+                                       (alist-get color color-mapping)
+                                     color)))
+           (funcall accessor theme-color)))
+        ((string-prefix-p "#" color) color)))
+
+
+
+(defun technicolor--color-to-hex (col)
+  "Convert a color COL 12 bit hexadecimal.
+
+COL can be a hexadecimal string of arbitrary bit depth or list of r g b
+color values."
+  (cond ((and col (listp col))
+         (pcase col
+           (`(,r ,g ,b) (color-rgb-to-hex r g b 2))))
+        ((string-prefix-p "#" col)
+         (technicolor--color-to-hex (color-name-to-rgb col)))))
+
+;;;###autoload
+(defun technicolor-darken (color alpha)
+  "Darken COLOR by ALPHA percent.
+
+COLOR can be a symbol in `technicolor-colors', a hexadecimal string, or list
+of either of the above."
+  (cond ((listp color)
+         (mapcar (lambda (col alpha) (technicolor-darken col alpha)) color))
+        ((and color (symbolp color))
+         (technicolor--color-to-hex
+          (color-darken-name
+           (technicolor-get-color color) alpha)))
+        ((string-prefix-p "#" color) (technicolor--color-to-hex (color-darken-name color alpha)))))
+
+;;;###autoload
+(defun technicolor-lighten (color alpha)
+  "Lighten COLOR by ALPHA percent.
+
+COLOR can be a symbol in `technicolor-colors', a hexadecimal string, or list
+of either of the above."
+  (technicolor-darken color (- alpha)))
+
+;;;###autoload
+(defun technicolor-complement (color)
+  "Return hexadecimal complement of COLOR."
+  (technicolor--color-to-hex (color-complement (technicolor-get-color color))))
+
+;;;###autoload
+(defun technicolor-gradient (start stop step-nums)
+  (let ((-start (technicolor-get-color start))
+        (-stop (technicolor-get-color stop)))
+    (mapcar #'technicolor--color-to-hex
+            (color-gradient (color-name-to-rgb -start) (color-name-to-rgb -stop) step-nums))))
+
+;;;###autoload
+(defun technicolor-saturate (color alpha)
+  "Saturate COLOR by ALPHA percent."
+  (technicolor--color-to-hex (color-saturate-name (technicolor-get-color color) alpha)))
+
+;;;###autoload
+(defun technicolor-desaturate (color alpha)
+  "Desturate COLOR by ALPHA percent."
+  (technicolor--color-to-hex (color-saturate-name (technicolor-get-color color) (- alpha))))
+
+;; TODO requiring `cl-lib' for just one function is overkill
+;; either lean into it or rework the function
+(require 'cl-lib)
+;; stolen from `doom-themes'
+(defun technicolor-blend (color1 color2 alpha)
+  "Blend two colors COLOR1 and COLOR2 by percentage ALPHA."
+  (when (and color1 color2)
+    (cond ((and color1 color2 (symbolp color1) (symbolp color2))
+           (technicolor-blend (technicolor-get-color  color1) (technicolor-get-color color2) alpha))
+          ((or (listp color1) (listp color2))
+           (cl-loop for x in color1
+                    when (if (listp color2) (pop color2) color2)
+                    collect (technicolor-blend x it alpha)))
+
+          ((and (string-prefix-p "#" color1) (string-prefix-p "#" color2))
+           (apply (lambda (r g b) (format "#%02x%02x%02x" (* r 255) (* g 255) (* b 255)))
+                  (cl-loop for it    in (color-name-to-rgb color1)
+                           for other in (color-name-to-rgb color2)
+                           collect (+ (* (/ alpha 100.0) it) (* other (- 1  (/ alpha 100.0))))))))))
+
 
 
 (provide 'technicolor)
